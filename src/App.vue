9dfbae9e8c3b98d03a6ee9dcaa4c423604cf1d59
123Joy123 <2506114287@qq.com>
2021-05-11 10:46:01 +0800

    底部导航栏完成

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,37 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view/>
+
+  <div id="nav">
+    <router-link class="tab-bar-item" to="/">
+      <div class="icon"><i class="iconfont icon-shouye"></i></div>
+      <div>
+        首页
+      </div>
+    </router-link>
+    <router-link class="tab-bar-item" to="/category">
+      <div class="icon"><i class="iconfont icon-biaoqian"></i></div>
+      <div>
+        分类
+      </div>
+    </router-link>
+    <router-link class="tab-bar-item" to="/shoppingcar">
+      <div class="icon"><i class="iconfont icon-gouwuche"></i></div>
+      <div>
+        购物车
+      </div>
+    </router-link>
+    <router-link class="tab-bar-item" to="/profile">
+      <div class="icon"><i class="iconfont icon-wo"></i></div>
+      <div>
+        我的
+      </div>
+    </router-link>
+  </div>
 </template>
 
 <style lang="scss">
 @import "./assets/css/base.css";
+@import "./assets/css/iconfont.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -16,16 +40,35 @@
   color: #2c3e50;
 }
 
+//底部导航栏样式
 #nav {
-  padding: 30px;
+  background-color: #f6f6f6;
+  display: flex;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  box-shadow: 0 -5px 1px rgba(100,100,100,0.1);
 
   a {
-    font-weight: bold;
-    color: #2c3e50;
+    color: var(--color-text);
 
     &.router-link-exact-active {
       color: var(--color-high-text);
     }
+  }
+  .tab-bar-item{
+    flex:1;
+    text-align: center;
+    height: 50px;
+    font-size: var(--font-size);
+  }
+  .tab-bar-item .icon{
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    display: inline-block;
+    vertical-align: middle;
   }
 }
 </style>
